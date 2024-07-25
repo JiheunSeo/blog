96c26823765aea4e9e1b90fa6dd3b49d4b705135
seojiheun <wlgmsaudqls@naver.com>
2024-07-25 13:37:59 +0900

    Feat : Redis Create, Read 완료

--- src/main/java/com/test/blog/BlogApplication.java
+++ src/main/java/com/test/blog/BlogApplication.java
@@ -2,10 +2,23 @@ package com.test.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 @SpringBootApplication
 public class BlogApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BlogApplication.class, args);
+		// Run the application and get the application context
+		ApplicationContext ctx = SpringApplication.run(BlogApplication.class, args);
+
+		// Get the StringRedisTemplate bean from the context
+		StringRedisTemplate template = ctx.getBean(StringRedisTemplate.class);
+
+		// Fetch and print the values from Redis
+		String val1 = template.opsForValue().get("testkey1");
+		System.out.println(val1);
+
+		// String val2 = template.opsForValue().get("testkey2");
+		// System.out.println(val2);
 	}
 }
